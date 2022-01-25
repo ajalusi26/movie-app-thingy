bbccec59f4449c2dc57b4dff2765d042f6f1c48f
AJ Jalusi <ajalusi26@gmail.com>
2022-01-25 15:56:38 -0500

    more updates

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -6,7 +6,7 @@ import MovieTile from "./MovieTile";
 import '../AllCss/MovieContainer.css'
 
 function MovieContainer({changeLoaded, moviesLoaded}){
-
+    const [searchValue, setSearchValue] = useState("")
     const [movieData, setMovieData] = useState([])
     const [moviesToDisplay, setMoviesToDisplay] = useState(0)
     useEffect(()=>{
@@ -16,13 +16,16 @@ function MovieContainer({changeLoaded, moviesLoaded}){
             setMovieData(data)
             changeLoaded()}
         )},[])
+
+    const filteredMovies = movieData.filter(item => item.Name.toLowerCase().includes(searchValue.toLowerCase()))
     
     if(moviesLoaded === true){
         return(
             <>
-                <Search/>
+                <Search setSearchValue={setSearchValue}/>
                 <div className="movie-container">
-                    {movieData.slice(moviesToDisplay, moviesToDisplay + 48)
+                    {console.log(movieData)}
+                    {filteredMovies.slice(moviesToDisplay, moviesToDisplay + 48)
                     .map(movie => <MovieTile movie={movie} key={movie.id}/>)}
                 </div>
             </>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,12 @@
 import React,{useState, useEffect} from "react";
 import '../AllCss/Search.css'
 
-function Search(){
+function Search({setSearchValue}){
+
+
     return(
         <div className="nav">
-            <input className="search" type="text" placeholder="search" size="50"/>
+            <input className="search" type="text" placeholder="search" size="50" onChange={(e) => setSearchValue(e.target.value)}/>
         </div>
     )
 
