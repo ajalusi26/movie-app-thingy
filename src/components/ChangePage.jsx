9d1acbe952ab96d360729ef81c14f24f7a706f39
AJ Jalusi <ajalusi26@gmail.com>
2022-01-26 15:04:52 -0500

    only css + user settings left

--- src/components/ChangePage.jsx
+++ src/components/ChangePage.jsx
@@ -5,13 +5,30 @@ import '../AllCss/ChangePage.css'
 
 
 function ChangePage({setMoviesToDisplay, moviesToDisplay}){
+
+    function backOnePage(){
+        if(moviesToDisplay === 0){
+            return
+        }else{
+            setMoviesToDisplay(moviesToDisplay - 48)
+        }
+    }
+    function upOnePage(){
+        if(moviesToDisplay === 192){
+            setMoviesToDisplay(0)
+        }else{
+            setMoviesToDisplay(moviesToDisplay + 48)
+        }
+    }
+
+
     return(
         
         <a href="#header">
             <div className="arrow-container">
                 <span className="arrows-span">
-                    <FontAwesomeIcon className="arrows" icon={faChevronLeft} onClick={() => setMoviesToDisplay(moviesToDisplay + 48)}></FontAwesomeIcon>
-                    <FontAwesomeIcon className="arrows" icon={faChevronRight} onClick={() => setMoviesToDisplay(moviesToDisplay + 48)}></FontAwesomeIcon>
+                    <FontAwesomeIcon className="arrows" icon={faChevronLeft} onClick={backOnePage}></FontAwesomeIcon>
+                    <FontAwesomeIcon className="arrows" icon={faChevronRight} onClick={upOnePage}></FontAwesomeIcon>
                 </span>                    
             </div>
         </a> 
